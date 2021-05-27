79f93deab7682f1b2b0a83f907e3b07ec6867422
jincheol5 <65759142+jincheol5@users.noreply.github.com>
2021-05-27 22:37:12 +0900

    Add pr04_07,08,09.py
    Add pr04_07,08,09.py

---
+++ 4.Python_조건문/실습코드/pr04_07.py
@@ -0,0 +1,5 @@
+s='my name is jincheol oh'
+
+n=int(input())
+if n==0: print(s[:11])
+if n==1: print(s[11:])
---
+++ 4.Python_조건문/실습코드/pr04_09.py
@@ -0,0 +1,13 @@
+t1=(1,2,'a','b')
+t2=('c','d',3,4)
+
+n=int(input())
+if n==0: print(t1+t2)
+if n==1:
+  a=int(input())
+  b=int(input())
+  print(t1[a-1])
+  print(t2[b-1])
+
+
+
