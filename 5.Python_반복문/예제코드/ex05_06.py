8ecb80c1498a447eee9c27648b8bf13d50eab055
JeongSangWon <59133541+jsw0402@users.noreply.github.com>
2021-05-27 14:14:05 +0900

    Add files via upload

---
+++ 5.Python_반복문/예제코드/ex05_06.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(i,end="")
+print()
+for i in range(4,10):
+    print(i,end="")
+print()
+for i in range(4,10,3):
+    print(i,end="")
---
+++ 5.Python_반복문/예제코드/ex05_09.py
@@ -0,0 +1,5 @@
+score=[95,80,100,90,95]
+total=0
+for i in score:
+    total+=i
+print("총점:{} 평균{}".format(total,total/5))
