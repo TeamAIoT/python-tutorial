683b33d2dcb699c17e3f0db2a2ad92dc5e91589f
JuWon Seo <43328935+JJuOn@users.noreply.github.com>
2021-07-17 12:14:59 +0900

    Delete pr07_01.py

--- pr07_01.py
+++
@@ -1,7 +0,0 @@
-f=open("C:/Users/owner/Desktop/7.Python_파일입출력/실습파일/실습.txt",'w')
-while True:
-    data=input("파일에 입력할 문구를 작성하시오:")
-    if data=="stop":
-        break
-    f.write(data+"\n")
-f.close()
